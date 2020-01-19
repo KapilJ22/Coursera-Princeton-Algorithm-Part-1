922bfa96c09825244019be05d3c03e6c6626bdf9
Kapil Jain <kapil.jain@ibm.com>
2020-01-19 16:50:55 -0500

    fixed some defects

--- MyTests.java
+++ MyTests.java
@@ -35,15 +35,15 @@ public class MyTests {
 
 
 
-    @Test
-    public void siteAddressMAppingFromRowAndAddressIsCorrect() {
-        Percolation tester = new Percolation(5); // MyClass is tested
-
-        // assert statements
-        //  assertEquals(false("blocked" tester.isOpen(2,1)));
-        //assertFalse("sites not blocked initially", tester.isOpen(2,1));
-assertEquals(6,tester.oneDimensionSiteAddress(1,1));
-    }
+//     @Test
+//     public void siteAddressMAppingFromRowAndAddressIsCorrect() {
+//         Percolation tester = new Percolation(5); // MyClass is tested
+//
+//         // assert statements
+//         //  assertEquals(false("blocked" tester.isOpen(2,1)));
+//         //assertFalse("sites not blocked initially", tester.isOpen(2,1));
+// assertEquals(6,tester.oneDimensionSiteAddress(1,1));
+//     }
 
 
     @Test
@@ -106,6 +106,28 @@ assertEquals(6,tester.oneDimensionSiteAddress(1,1));
         // tester.open(5,1);
 
         assertEquals("4 sites open",4,tester.numberOfOpenSites());
+    }
+
+    @Test
+    public void iSSiteFull() {
+        Percolation tester = new Percolation(6); // MyClass is tested
+
+        // assert statements
+        //  assertEquals(false("blocked" tester.isOpen(2,1)));
+        tester.open(1,6);
+        tester.open(2,6);
+        tester.open(3,6);
+        tester.open(4,6);
+        tester.open(5,6);
+        tester.open(5,5);
+        tester.open(4,4);
+        tester.open(3,4);
+        tester.open(2,4);
+        tester.open(2,3);
+        tester.open(2,2);
+        tester.open(2,1);
+        assertFalse("sites is full", tester.isFull(2,1));
+
     }
 
 }
--- Percolation.java
+++ Percolation.java
@@ -1,96 +1,33 @@
-import edu.princeton.cs.algs4.QuickFindUF;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class Percolation {
-    private int nSquare;
-    private int n;
+    final private int nSquare;
+    final private int n;
     private boolean[] openSites;
     private boolean[] fullSites;
-    private QuickFindUF quickFindUF;
+    private WeightedQuickUnionUF quickFindUF;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n <= 0) throw new IllegalArgumentException("N must be > 0");
         this.n = n;
         nSquare = n * n;
-        quickFindUF = new QuickFindUF(nSquare + 2);
+        quickFindUF = new WeightedQuickUnionUF(nSquare + 2);
         openSites = new boolean[nSquare + 2];
         fullSites = new boolean[nSquare + 2];
-        for (int i = 0; i < nSquare + 2; i++) {
+        for (int i = 0; i < nSquare; i++) {
             openSites[i] = false;
         }
-        for (int i = 0; i < nSquare + 2; i++) {
+        openSites[nSquare] = true;
+        openSites[nSquare + 1] = true;
+
+        for (int i = 0; i < nSquare; i++) {
             fullSites[i] = false;
         }
-    }
+        fullSites[nSquare] = true;
+        fullSites[nSquare + 1] = true;
 
-    public int oneDimensionSiteAddress(int row, int col) {
-
-        return row * n + col;
-
-    }
-
-    // opens the site (row, col) if it is not open already
-    public void open(int row, int col) {
-
-        openSites[oneDimensionSiteAddress(row - 1, col - 1)] = true;
-        unionAdjacent((row - 1) * n + (col - 1));
-
-    }
-
-    // is the site (row, col) open?
-    public boolean isOpen(int row, int col) {
-        return openSites[oneDimensionSiteAddress(row - 1, col - 1)];
-    }
-
-    // is the site (row, col) full?
-    public boolean isFull(int row, int col) {
-        return fullSites[oneDimensionSiteAddress(row - 1, col - 1)];
-    }
-
-    // returns the number of open sites
-    public int numberOfOpenSites() {
-        int count = 0;
-        for (int i = 0; i < nSquare; i++) {
-            if (isOpen((i / n) + 1, (i % n) + 1)) count++;
-        }
-        return count;
-    }
-
-    // does the system percolate?
-    public boolean percolates() {
-        if (quickFindUF.connected(nSquare, nSquare + 1))
-            return true;
-        else return false;
-    }
-
-    private void unionAdjacent(int x) {
-        int y = nSquare;
-        if (x >= 0 && x < n)
-            quickFindUF.union(x, y);
-
-        y = nSquare + 1;
-        if (x >= nSquare - n && x <= nSquare)
-            quickFindUF.union(x, y);
-
-        y = x - 1;
-        unionAdjacents(x, y);
-
-        y = x + 1;
-        unionAdjacents(x, y);
-
-        y = x - n;
-        unionAdjacents(x, y);
-
-        y = x + n;
-        unionAdjacents(x, y);
-    }
-
-    private void unionAdjacents(int x, int y) {
-        if (y >= 0 && y <= nSquare - 1) {
-            if (isOpen((y / n) + 1, (y % n) + 1))
-                quickFindUF.union(x, y);
-        }
     }
 
     // test client (optional)
@@ -130,5 +67,92 @@ public class Percolation {
         //             x1 + "," + x2 + "connected? " + percolate.quickFindUF.connected(x1, x2));
         //
         // }
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+
+        openSites[oneDimensionSiteAddress(row - 1, col - 1)] = true;
+        unionAdjacent((row - 1) * n + (col - 1));
+
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        return openSites[oneDimensionSiteAddress(row - 1, col - 1)];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        return quickFindUF.connected(
+                oneDimensionSiteAddress(row - 1, col - 1),
+                nSquare
+        );
+    }
+
+    private int oneDimensionSiteAddress(int row, int col) {
+
+        return row * n + col;
+
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        int count = 0;
+        for (int i = 0; i < nSquare; i++) {
+            if (isOpen((i / n) + 1, (i % n) + 1)) count++;
+        }
+        return count;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        if (quickFindUF.connected(nSquare, nSquare + 1))
+            return true;
+        else return false;
+    }
+
+    private void unionAdjacent(int x) {
+        int y = nSquare;
+        if (x >= 0 && x < n) {
+            quickFindUF.union(x, y);
+        //    setSitesFull(x, y);
+        }
+        y = nSquare + 1;
+        if (x >= nSquare - n && x < nSquare) {
+            quickFindUF.union(x, y);
+         //   setSitesFull(x, y);
+        }
+
+        if(!(x%n==0)) {
+            y = x - 1;
+            unionAdjacents(x, y);
+        }
+
+        if(!((x+1)%n==0)) {
+            y = x + 1;
+            unionAdjacents(x, y);
+        }
+        y = x - n;
+        unionAdjacents(x, y);
+
+        y = x + n;
+        unionAdjacents(x, y);
+    }
+
+    private void setSitesFull(int x, int y) {
+        // if (quickFindUF.connected(x, nSquare)) {
+        //     this.fullSites[x] = true;
+        //     this.fullSites[y] = true;
+        // }
+    }
+
+    private void unionAdjacents(int x, int y) {
+        if (y >= 0 && y <= nSquare - 1) {
+            if (isOpen((y / n) + 1, (y % n) + 1)) {
+                quickFindUF.union(x, y);
+              //  setSitesFull(x, y);
+            }
+        }
     }
 }
